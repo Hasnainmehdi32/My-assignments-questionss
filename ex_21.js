9d30fc13cc0e22e47b03e2bc3f70a68987cb801e
Hasnain312 <165997783+Hasnainmehdi32@users.noreply.github.com>
2024-04-14 14:42:36 +0500

    first commit

---
+++ ex_21.js
@@ -0,0 +1,12 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+// object is Consist of key + Value
+let stdDitails = {
+    name: "Ali",
+    // key value 
+    age: 18,
+    gender: "male",
+    rollNo: 546357,
+};
+console.log(stdDitails);
---
+++ ex_21.ts
@@ -0,0 +1,13 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+// object is Consist of key + Value
+let stdDitails = {
+    name:"Ali",
+    // key value 
+    age: 18,
+    gender: "male",
+    rollNo: 546357,
+    
+};
+console.log(stdDitails);
+
